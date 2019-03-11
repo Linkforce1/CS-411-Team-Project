b222efe3bcd3ab2cdecbf06002679ac6ae2a1ee8
ziyic2 <ziyic2@illinois.edu>
2019-03-10 20:26:28 -0500

    adding welcome, signup, and login page

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,9 @@ from main import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("welcome", views.welcome, name="welcome"),
+    path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
     path("main/<name>", views.hello_there, name="hello_there"),
     path("", views.hello_world, name = "hello_world"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,17 @@ import re
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
+from django import forms
+from django.http import HttpResponseRedirect
+class login_form(forms.Form):
+    email = forms.CharField(label='Email', max_length=100)
+    password = forms.CharField(label='password', max_length=100)
+
+class sign_up_form(forms.Form):
+    email = forms.CharField(label='Email', max_length=100)
+    user_name = forms.CharField(label='User name', max_length=100)
+    password = forms.CharField(label='password', max_length=100)
+    confirm_password = forms.CharField(label='confirm password', max_length=100)
 
 def home(request):
     return HttpResponse("Hello, Django!")
@@ -18,3 +29,33 @@ def hello_there(request, name):
 
 def hello_world(request):
     return HttpResponse("Hello World!, this is my first Web Appliction!")
+
+def login(request):
+    if request.method == 'POST':
+        form = login_form(request.POST)
+        if form.is_valid():
+            return HttpResponse('Done!')
+    else:
+        form = login_form()
+    return render(
+        request,
+        'login.html', {'form':form}
+    )
+
+def signup(request):
+    if request.method == 'POST':
+        form = sign_up_form(request.POST)
+        if form.is_valid():
+            return HttpResponse('Done!')
+    else:
+        form = sign_up_form()
+    return render(
+        request,
+        'login.html', {'form':form}
+    )
+
+def welcome(request):
+    return render(
+        request,
+        'welcome.html'
+    )
